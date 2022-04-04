ad4ce85342965caef61ea95d78da229265473d1a
ThinkingDobby <chiho1221@naver.com>
2022-04-04 23:12:26 +0900

    First Commit

---
+++ app/src/main/java/com/thinkingdobby/tukusedbook/LoadingActivity.kt
@@ -0,0 +1,30 @@
+package com.thinkingdobby.tukusedbook
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_loading.*
+
+class LoadingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+
+        window.apply {
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                statusBarColor = Color.TRANSPARENT
+            }
+        }
+
+        loading_v_start.setOnClickListener {
+            val intent = Intent(this, CreateProfileActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
