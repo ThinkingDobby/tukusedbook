81baef60bfe92038aa97c40bf5dc0b176d771e96
ThinkingDobby <chiho1221@naver.com>
2022-04-20 22:54:59 +0900

    액티비티 생성

---
+++ app/src/main/java/com/thinkingdobby/tukusedbook/MyBookActivity.kt
@@ -0,0 +1,12 @@
+package com.thinkingdobby.tukusedbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyBookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_book)
+    }
+}
